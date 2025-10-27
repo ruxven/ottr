e1b205b0e8e9fb7b42c828e2d27faf9b7d33075a
ruxven <ruxven@protonmail.ch>
2025-10-27 18:59:02 -0400

    Corrected csv_to_json converter for complexity task

--- .github/scripts/csv_to_json_complexity.py
+++ .github/scripts/csv_to_json_complexity.py
@@ -18,22 +18,22 @@ def convert_csv_to_json(csv_file, json_file):
         # Group functions by file
         files = {}
         for row in reader:
-            filename = row.get('filename', '')
+            filename = row.get('file', '')
             if filename not in files:
                 files[filename] = {
-                    'filename': filename,
+                    'file': filename,
                     'function_list': []
                 }
             
             # Extract function information
             function_info = {
-                'name': row.get('function_name', ''),
+                'name': row.get('function', ''),
                 'cyclomatic_complexity': int(row.get('CCN', 0)),
                 'start_line': int(row.get('start_line', 0)),
                 'end_line': int(row.get('end_line', 0)),
                 'nloc': int(row.get('NLOC', 0)),
-                'token_count': int(row.get('token_count', 0)),
-                'parameter_count': int(row.get('parameter_count', 0))
+                'token_count': int(row.get('token', 0)),
+                'parameter_count': int(row.get('PARAM', 0))
             }
             
             files[filename]['function_list'].append(function_info)
